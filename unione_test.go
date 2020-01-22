dd2b521409999ee9f27359433bd0576e6906f580
Alexey Popov <me@alexeyco.dev>
2020-01-22 17:20:03 +0300

    Added message sending example

--- unione_test.go
+++ unione_test.go
@@ -2,6 +2,7 @@ package unione_test
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"testing"
 
@@ -47,4 +48,21 @@ func TestClient_Send(t *testing.T) {
 
 	expectedJson := `{"username":"foo","api_key":"bar","message":{"from_name":"John Doe","from_email":"foo@bar.example","recipients":[{"email":"recipient@example.com"}],"subject":"Lorem ipsum","body":{"html":"Novus ordo seclorum"}}}`
 	utils.JsonIsEqual(t, expectedJson, givenJson)
+}
+
+func ExampleClient_Send() {
+	recipient := message.NewRecipient("recipient@example.com").
+		Name("John Doe")
+
+	msg := message.NewMessage().
+		From("site@example.com", "My site").
+		To(recipient).
+		Subject("Awesome news, buddy").
+		BodyPlainText("Return to my site and enjoy")
+
+	client := unione.New("username", "api-key")
+
+	if err := client.Send(msg); err != nil {
+		log.Fatalln(err)
+	}
 }
